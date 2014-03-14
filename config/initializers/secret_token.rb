2993a52b303034fcb9a468a3ed80a51e5118fc66
Essam Joubori <essam.joubori@gmail.com>
2014-03-14 16:33:20 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EKalltower::Application.config.secret_key_base = '87fc762a338f0ef9f569696154497db9e8fc32af9496a5bbc9b12e7389fb9940eebb593289f9046cbec69ff0547e29f2a58b568ef31167a3bcea3be978f21c57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EKalltower::Application.config.session_store :cookie_store, key: '_e-kalltower_session'
