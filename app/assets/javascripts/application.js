ba9c39541150b757a0e8cb21f8cbdec10ea8fcf0
Essam Joubori <essam.joubori@gmail.com>
2014-04-10 22:47:49 -0500

    Fixed Lightbox bug and added JQ arrows to h3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,11 +16,6 @@
 //= require_tree .
 //= require fancybox
 
-// var myVar = "red";
-
-// console.log(typeof myVar === "undefined");
-// console.log(myVar === undefined);
-
 
 $(document).ready(function(){
 	$( "h3" ).click(function() {
@@ -38,8 +33,39 @@ $('.glow').click(function(){
 
 
 
-/* FancyBox 
+/* LightBox 
 ================= */
+//Capture the click event on a link to an image
+
+// $(document).ready(function(){
+// 	//Add overlay
+// 	var $overlay = $('<div id="overlay"></div>');
+// 	//Add image
+// 	var $image = $("<img>");
+// 	$overlay.append($image);
+// 	
+// 	
+// 	$("body").append($overlay);	
+// 
+// 	
+// 	$(".gallery a").click(function(event){
+// 		event.preventDefault();
+// 		var imageLocation = $(this).attr("href");
+// 		$image.attr("src", imageLocation);	
+// 		$overlay.show();
+// 	  });
+// 	  $overlay.click(function(){
+// 		  $(this).hide();
+// 	  });	
+// });
+//Show the overlay
+//Update overlay with the image linked in the link
+//Add the overlay
+//An image
+//When clicked on overlay, hide the overlay
+ 
+
+
 
 $(document).ready(function() {
 	$(".fancybox").fancybox({
@@ -47,4 +73,78 @@ $(document).ready(function() {
 		closeEffect	: 'none'
 	});
 });
+
+/* Add arrows when hover over h3 links
+===================================== */
+
+
+
+$(document).ready(function() {
+
+//Define two arrows images, one points right and other down
+var $arrowRight = $('<div id="arrowright"></div>');
+var $arrowRightAbout = $('<div id="arrowright_about"></div>');
+var $arrowRightShort = $('<div id="arrowright_short"></div>');
+
+
+// Triger the hover function when move mouse to h3
+// Show an arrow pointing to the right when hover
+  	$(".arrow").hover(function(){
+	$(this).prepend($arrowRight);}, function(){
+	$arrowRight.remove();
+	});
+	
+	
+  	$(".arrow_about").hover(function(){
+	$(this).prepend($arrowRightAbout);}, function(){
+	$arrowRightAbout.remove();
+	});
+	
+  	$(".arrow_short").hover(function(){
+	$(this).prepend($arrowRightShort);}, function(){
+	$arrowRightShort.remove();
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
+/*Menu drop list for mobile
+============================= */
+
+
+//Create a select and appent-to menu
+// $(document).ready(function(){
+// 	var $select = $("<select></select>");
+// 	$("#nav").append($select);	
+// 	
+// });
+
+
+	//Cycle over menu links 
+	//Create an options
+	//Options value of href
+	//Option's text is the text of link
+	//Append option to select
+//Create button 
+//Bind click  
+  //go back to select location
+  
+//Modify CSS to hide links on small screens and show button instead
+//Also hides select and button on large screens and show the links
 
